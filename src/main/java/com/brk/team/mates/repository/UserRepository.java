4db523dd501b2a9bee0d25b3d0068f61d29523e8
Burak Alparslan <burak.alparslan@monitise.com>
2016-11-21 17:26:02 +0300

    Added RepositoryRestResource and security example.

--- src/main/java/com/brk/team/mates/repository/UserRepository.java
+++ src/main/java/com/brk/team/mates/repository/UserRepository.java
@@ -2,11 +2,15 @@ package com.brk.team.mates.repository;
 
 import com.brk.team.mates.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
+@RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepository extends CrudRepository<User, Long> {
 
+    @PreAuthorize(value = "isAuthenticated()")
     List<User> findAll();
     
     List<User> findByFirstNameAndLastName(String firstName, String lastName);
