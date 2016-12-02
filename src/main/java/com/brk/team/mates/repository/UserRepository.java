85915dd465708f3b80e9f4053f938100f3f284fe
Burak Alparslan <burak.alparslan@monitise.com>
2016-12-02 11:38:48 +0300

    Added params for user repository.

--- src/main/java/com/brk/team/mates/repository/UserRepository.java
+++ src/main/java/com/brk/team/mates/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.brk.team.mates.repository;
 
 import com.brk.team.mates.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 
@@ -12,8 +13,6 @@ public interface UserRepository extends CrudRepository<User, Long> {
 
     @PreAuthorize(value = "isAuthenticated()")
     List<User> findAll();
-    
-    List<User> findByFirstNameAndLastName(String firstName, String lastName);
 
-    User findByUserName(String userName);
+    User findByUserName(@Param(value = "userName") String userName);
 }
